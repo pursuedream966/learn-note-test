4b0a92f81f1bf7c96353c2a85069a193b0e4e248
hyy <hyy_study@163.com>
2022-07-18 14:13:59 +0800

    feat(tooMany): 模块重构

---
+++ common-lib/src/main/java/cn/pursuedream/commonlib/utils/CommonConstant.java
@@ -0,0 +1,6 @@
+package cn.pursuedream.commonlib.utils;
+
+public class CommonConstant {
+    public static final Integer SUCCESS = 0;
+    public static final Integer ERROR = 1;
+}
---
+++ common-lib/src/main/java/cn/pursuedream/commonlib/utils/Result.java
@@ -0,0 +1,46 @@
+package cn.pursuedream.commonlib.utils;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Builder
+public class Result <T> implements Serializable {
+    private Integer code;
+    private String msg;
+    private T data;
+
+    public static <T> Result<T> success(){
+        return new Result<>(CommonConstant.SUCCESS, null, null);
+    }
+
+    public static <T> Result<T> success(String msg){
+        return new Result(CommonConstant.SUCCESS, msg, null);
+    }
+
+    public static <T> Result<T> success(String msg, T t){
+        return new Result(CommonConstant.SUCCESS, msg, t);
+    }
+
+    public static <T> Result<T> failed(){
+        return new Result<>(CommonConstant.ERROR, null, null);
+    }
+
+    public static <T> Result<T> failed(String msg){
+        return new Result<>(CommonConstant.ERROR, msg, null);
+    }
+
+    public static <T> Result<T> failed(String msg, T t){
+        return new Result<>(CommonConstant.ERROR, msg, t);
+    }
+
+    public static <T> Result<T> failed(Integer code, String msg, T t){
+        return new Result<>(code, msg, t);
+    }
+}
