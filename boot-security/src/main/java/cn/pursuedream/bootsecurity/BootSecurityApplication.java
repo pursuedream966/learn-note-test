ef0401915d36dc94196a4cca3912897a28910e76
hyy <hyy_study@163.com>
2022-07-18 16:00:32 +0800

    feat(tooMany): security测试代码

--- boot-security/src/main/java/cn/pursuedream/bootsecurity/BootSecurityApplication.java
+++ boot-security/src/main/java/cn/pursuedream/bootsecurity/BootSecurityApplication.java
@@ -6,6 +6,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 /**
  * security调用原理
  * https://juejin.cn/post/7046236598832201758
+ *
+ * 新版security注意事项
+ * https://spring.io/blog/2022/02/21/spring-security-without-the-websecurityconfigureradapter
  */
 @SpringBootApplication
 public class BootSecurityApplication {
--- boot-security/src/main/java/cn/pursuedream/bootsecurity/config/SecurityConfig.java
+++ boot-security/src/main/java/cn/pursuedream/bootsecurity/config/SecurityConfig.java
@@ -3,17 +3,51 @@ package cn.pursuedream.bootsecurity.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
 
     @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+    @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.authorizeRequests()
-                .antMatchers("/security/**").permitAll();
+                .anyRequest().authenticated()
+                .and().formLogin().defaultSuccessUrl("/security/hello")
+                .permitAll()
+        ;
 
         return http.build();
     }
+
+    @Bean
+    public WebSecurityCustomizer webSecurityCustomizer(){
+        return new WebSecurityCustomizer() {
+            @Override
+            public void customize(WebSecurity web) {
+                web.ignoring().antMatchers("/ignore/**");
+            }
+        };
+    }
+
+    @Bean
+    public InMemoryUserDetailsManager userDetailsService(PasswordEncoder passwordEncoder){
+        UserDetails userDetails = User.withUsername("admin")
+                .password(passwordEncoder.encode("123456"))
+                .roles("ADMIN")
+                .build();
+        return new InMemoryUserDetailsManager(userDetails);
+    }
+
 
 }
